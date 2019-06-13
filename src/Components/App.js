d5647c48b27e7e49cbdf62dd2437dd66af04e1ec
Lawrence Luo <luolawrence1@gmail.com>
2019-06-13 13:00:17 -0700

    added Google Analytics, dev https

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,19 @@
 import '../css/App.css';
 import React from 'react';
+import ReactGA from 'react-ga';
 
 import Main from './main';
 import Footer from './footer';
 import Header from './header';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize Google Analytics to track pageviews
+    ReactGA.initialize('UA-142139610-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
   render() {
     return(
       <div className="appContainer">
