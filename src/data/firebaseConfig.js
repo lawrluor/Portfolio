68ac375bc3989e60be0cb453993d421fc78eabb2
Lawrence Luo <luolawrence1@gmail.com>
2022-02-17 09:54:30 -0800

    added measurementID for Firebase analytics

--- src/data/firebaseConfig.js
+++ src/data/firebaseConfig.js
@@ -5,7 +5,8 @@ const firebaseConfig = {
     projectId: "portfolio-2be56",
     storageBucket: "",
     messagingSenderId: "639657391884",
-    appId: "1:639657391884:web:761162e2ff35f587"
-  };
+    appId: "1:639657391884:web:761162e2ff35f587",
+    measurementId: "G-Q3M6MM5Q6N"
+};
 
 export default firebaseConfig;
